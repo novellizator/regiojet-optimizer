9173ad998c07e74da0c25259a4b8e7cb4586af64
tomasn <tomasnovella@gmail.com>
2020-12-23 18:35:48 +0100

    Timetable

--- src/services/timetableService.ts
+++ src/services/timetableService.ts
@@ -1,21 +1,18 @@
-import { LocationsProvider } from "../locationsProvider";
-import { RouteId } from "../types/routeSearchRoutes";
-import { Timetable } from "../types/timetable";
+import axios from 'axios'
+import { RouteId } from "../types/routeSearchRoutes"
+import { Timetable } from "../types/timetable"
 
 import timetable from '../mocks/timetables.json'
 
 interface TimetableFetching {
     fetchTimetableForRoute(route: RouteId): Promise<Timetable>
 }
-// class TimetableService implements TimetableFetching {
-//     constructor(private locationsProvider: LocationsProvider) {
-//     }
-
-//     fetchTimetableForRoute = (route: RouteId) => {
-//         return fetch("https://brn-ybus-pubapi.sa.cz/restapi/consts/timetables/" + route)
-//         .then(req => req.json()) as Timetable
-//     }
-// }
+class TimetableService implements TimetableFetching {
+    fetchTimetableForRoute = (route: RouteId): Promise<Timetable> => {
+        const uri = "https://brn-ybus-pubapi.sa.cz/restapi/consts/timetables/" + route
+        return axios.get(uri).then(response => response.data)
+    }
+}
 
 export class MockTimetableService implements TimetableFetching {
     fetchTimetableForRoute = async (routeId: RouteId) => {
